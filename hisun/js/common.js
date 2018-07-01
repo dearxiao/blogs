f9d8b96a31143b5abfe1eadaf2e7e69cdeb75784
ming <362542883@qq.com>
2018-07-01 16:13:04 +0800

    first

---
+++ hisun/js/common.js
@@ -0,0 +1,58 @@
+onload = function () {
+    layui.use(['element', 'carousel'], function(){
+        var element = layui.element, carousel = layui.carousel
+        carousel.render({ //轮播
+            elem: '#test1',
+            width: '100%' ,
+            height: '900px',
+        });
+        carousel.render({ //动物健康
+			elem: '#test2',
+			width: '100%' ,
+			height: '420px',
+			autoplay: false,
+			arrow: 'always',
+			indicator: 'none'
+		});
+        carousel.render({
+			elem: '#test3',
+			width: '100%' ,
+			height: '420px',
+			autoplay: false,
+			arrow: 'always',
+			indicator: 'none'
+		});
+        carousel.render({ //社会责任
+			elem: '#test4',
+			width: '100%' ,
+			height: '420px',
+			autoplay: false,
+			arrow: 'always',
+			indicator: 'none'
+		});
+        carousel.render({
+			elem: '#test5',
+			width: '100%' ,
+			height: '420px',
+			autoplay: false,
+			arrow: 'always',
+			indicator: 'none'
+		});
+        carousel.render({
+			elem: '#test6',
+			width: '100%' ,
+			height: '420px',
+			autoplay: false,
+			arrow: 'always',
+			indicator: 'none'
+		});
+        carousel.render({ //关于我们
+			elem: '#test7',
+			width: '100%' ,
+			height: '420px',
+			autoplay: false,
+			arrow: 'always',
+			indicator: 'none'
+		});
+    })
+}
