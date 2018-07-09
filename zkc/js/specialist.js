141c30e2f78adf36b03375caf6ad424eac50a235
ming <362542883@qq.com>
2018-07-09 19:21:55 +0800

    响应式着急版

---
+++ zkc/js/specialist.js
@@ -0,0 +1,26 @@
+
+var width = document.body.clientWidth,
+    num = 5
+if (width < 1200) {
+    num = 3
+}
+if (width < 760) {
+    num = 1
+}
+var specialist = new Swiper('.specialist', {
+    loop: true,
+    slidesPerView: num,
+    spaceBetween: 40,
+    // autoplay: true,
+
+    // 如果需要分页器
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // 如果需要前进后退按钮
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
