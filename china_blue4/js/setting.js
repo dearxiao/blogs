04b4091063637dc0139983795047a4ac20409ed1
ming <362542883@qq.com>
2018-12-01 17:22:19 +0800

    17：22

--- china_blue4/js/setting.js
+++ china_blue4/js/setting.js
@@ -1,13 +1,13 @@
-var phoneWidth =  parseInt(window.screen.width);
-var phoneScale = phoneWidth/640;
+var phoneWidth = parseInt(window.screen.width);
+var phoneScale = phoneWidth / 640;
 var ua = navigator.userAgent;
-if (/Android (\d+\.\d+)/.test(ua)){
+if (/Android (\d+\.\d+)/.test(ua)) {
     var version = parseFloat(RegExp.$1);
     // andriod 2.3
-    if(version>2.3){
-        document.write('<meta name="viewport" content="width=640, minimum-scale = '+phoneScale+', maximum-scale = '+phoneScale+', target-densitydpi=device-dpi">');
+    if (version > 2.3) {
+        document.write('<meta name="viewport" content="width=640, minimum-scale = ' + phoneScale + ', maximum-scale = ' + phoneScale + ', target-densitydpi=device-dpi">');
         // andriod 2.3以上
-    }else{
+    } else {
         document.write('<meta name="viewport" content="width=640, target-densitydpi=device-dpi">');
     }
     // 其他系统
@@ -15,7 +15,7 @@ if (/Android (\d+\.\d+)/.test(ua)){
     document.write('<meta name="viewport" content="width=640, user-scalable=no">');
 }
 //微信去掉下方刷新栏
-if(RegExp("MicroMessenger").test(navigator.userAgent)){
+if (RegExp("MicroMessenger").test(navigator.userAgent)) {
     document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
         WeixinJSBridge.call('hideToolbar');
     });
